2954e8fdcaaf742bbabd208610f20e650f875c47
Firebreath15 <hadengw@gmail.com>
2013-09-14 12:39:37 -0600

    Eclipse commit test

--- Quicksand/src/me/firebreath15/quicksand/Delta.java
+++ Quicksand/src/me/firebreath15/quicksand/Delta.java
@@ -1,7 +1,7 @@
 package me.firebreath15.quicksand;
 
 public class Delta {
-	
+	//
 	public boolean ifChangeWasOne(int oldx, int newx){		
 		if(newx-oldx==1 || oldx-newx==1 || newx-oldx==-1 || oldx-newx==-1){
 			return true;
